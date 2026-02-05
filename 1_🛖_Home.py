df037e1031c2676ecb85b152f2c541e5c4e90d60
LuizDomingo27 <luiz-elayny@live.com>
2026-02-05 20:21:57 -0300

    Adicionado o recurso de tabs

--- 1_🛖_Home.py
+++ 1_🛖_Home.py
@@ -10,30 +10,43 @@ st.set_page_config(
   layout="wide",
   initial_sidebar_state= "expanded"
 )
-
 # Criando o titulo
-st.markdown('# Data Analitics Futbol Europeu')
+st.markdown('# LD Data Analitics ⚽')
+tab = st.tabs(['Europa','Brasileirão'])
 
-# Carregando os dados
-df = JogosPorEquipes.SearchGame()
+# Tab com jogos Europeu
+with tab[0]:
+  st.markdown('# Data Analitics Futbol Europeu')
+  col1, col2 = st.columns(2, vertical_alignment= 'top', width= 520)
+  df = JogosPorEquipes.SearchGame()
+  ligas = sorted(df['Season'].unique())
+ 
+  with col1:
+    liga = st.selectbox('Ligas', ligas)
 
-# Criando a variável para armazenar as ligas
-ligas = df['Season'].unique()
-liga = st.sidebar.selectbox('Ligas', ligas)
+  df_equipes = df[df['Season'] == liga]
+  equipes = sorted(df_equipes['Home'].unique())
+  
+  with col2:
+    equipe = st.selectbox('Equipes', equipes)
 
-# Criando a variavél para filtrar as equipes de acordo com aliga selecionada
-df_equipes = df[df['Season'] == liga]
-equipes = df_equipes['Home'].unique()
-equipe = st.sidebar.selectbox('Equipes', equipes)
+  # Criando o DataFrame de acordo com a equipe selecionada
+  df_jogos = df_equipes[(df_equipes['Home'] == equipe)|(df_equipes['Away'] == equipe)].set_index("Home")
+  st.divider()
+  st.write(f"Total de Jogos {df_jogos['Away'].count()}")
+  st.dataframe(df_jogos.iloc[:, 2:-1], width="stretch") 
 
-# Criando o DataFrame de acordo com a equipe selecionada
-df_jogos = df_equipes[(df_equipes['Home'] == equipe)|(df_equipes['Away'] == equipe)].set_index("Home")
-st.markdown(f"## {equipe}")
-st.divider()
-st.write(f"Total de Jogos {df_jogos['Away'].count()}")
-st.dataframe(df_jogos.iloc[:, 2:-1], width="stretch") 
+  # Criando o botal para navegar até o perfil
+  st.sidebar.markdown("#### Developement by *Luiz Domingo*")
+  btn = st.sidebar.link_button("Perfil","https://www.linkedin.com/in/luiz-domingo-silva-dev-dev/")
 
-# Criando o botal para navegar até o perfil
-st.sidebar.markdown("#### Developement by *Luiz Domingo*")
-btn = st.sidebar.link_button("Perfil","https://www.linkedin.com/in/luiz-domingo-silva-dev-dev/")
+
+# Tab com Jogos do brasileirão
+with tab[1]:
+  dfb = JogosPorEquipes.SearcJogosBR()
+  st.markdown('# Data Analitics Brasileirão')
+
+  equipes_br = sorted(dfb['Home'].unique())
+  equipebr = st.selectbox('Equipe', equipes_br, width=150)
+
 
--- Model/JogosPorEquipes.py
+++ Model/JogosPorEquipes.py
@@ -10,3 +10,12 @@ def SearchGame():
     st.session_state.data = df
     df_jogos = st.session_state.data 
     return df_jogos
+
+
+@st.cache_data(ttl=1800)
+def SearcJogosBR():
+  if "data" not in st.session_state:
+    dfb = pd.read_csv('datasets/BR.csv')
+    st.session_state.data = dfb
+    dfbr = st.session_state.data
+    return dfbr
