7f9de3370046a4b5c38affc24608fad017e15291
LuizDomingo27 <luiz-elayny@live.com>
2026-02-05 18:51:57 -0300

    Correcao do titulo

--- 1_🛖_Home.py
+++ 1_🛖_Home.py
@@ -12,31 +12,28 @@ st.set_page_config(
 )
 
 # Criando o titulo
-st.markdown('# Data Analitics')
+st.markdown('# Data Analitics Futbol Europeu')
 
 # Carregando os dados
 df = JogosPorEquipes.SearchGame()
 
-
 # Criando a variável para armazenar as ligas
 ligas = df['Season'].unique()
 liga = st.sidebar.selectbox('Ligas', ligas)
-
 
 # Criando a variavél para filtrar as equipes de acordo com aliga selecionada
 df_equipes = df[df['Season'] == liga]
 equipes = df_equipes['Home'].unique()
 equipe = st.sidebar.selectbox('Equipes', equipes)
 
-
 # Criando o DataFrame de acordo com a equipe selecionada
 df_jogos = df_equipes[(df_equipes['Home'] == equipe)|(df_equipes['Away'] == equipe)].set_index("Home")
 st.markdown(f"## {equipe}")
 st.divider()
 st.write(f"Total de Jogos {df_jogos['Away'].count()}")
-st.dataframe(df_jogos.iloc[:, 2:], width="stretch") 
-
+st.dataframe(df_jogos.iloc[:, 2:-1], width="stretch") 
 
 # Criando o botal para navegar até o perfil
 st.sidebar.markdown("#### Developement by *Luiz Domingo*")
 btn = st.sidebar.link_button("Perfil","https://www.linkedin.com/in/luiz-domingo-silva-dev-dev/")
+
--- Model/JogosPorEquipes.py
+++ Model/JogosPorEquipes.py
@@ -3,9 +3,10 @@ import streamlit as st
 
 
 #%%
-@st.cache_data
+@st.cache_data(ttl=1800)
 def SearchGame():
   if "data" not in st.session_state:
-    df_jogos = pd.read_csv('datasets/Dados.csv')
-    st.session_state["data"] = df_jogos
+    df = pd.read_csv('datasets/Dados.csv')
+    st.session_state.data = df
+    df_jogos = st.session_state.data 
     return df_jogos
